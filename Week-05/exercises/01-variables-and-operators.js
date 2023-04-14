8bc2135914f887e4502f0f607055f3444e0d9c1d
Walter Ariel Pavicich <walterarielpavicich@gmail.com>
2023-04-14 11:22:10 -0300

    BaSP-W05-2023: variables and operators

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,33 @@
+// Variables and operators
+
+//  a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
+//     de ambos números en una 3er variable.
+
+var firstNumber = 7;
+var secondNumber = 9;
+
+var resultadoSuma = firstNumber + secondNumber;
+
+console.log('Ejercicio 1.a');
+console.log(resultadoSuma);
+
+//  b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var nombre = 'Walter Ariel';
+var apellido = ' Pavicich';
+
+var nombreCompleto = nombre + apellido;
+
+console.log('Ejercicio 1.b');
+console.log(nombreCompleto);
+
+//  c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+//     guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var firstSting = 'Radium';
+var secondString = 'Rocket';
+
+var sumaStrings = firstSting.length + secondString.length;
+
+console.log('Ejercicio 1.c');
+console.log(sumaStrings);
